7cb56cdeda3edda3eff9f60a4c9dc08f369eb2be
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-09 23:21:29 +0200

    Remove `api` middleware group

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,11 +24,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-
-        'api' => [
-            'throttle:api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
     ];
 
     protected $routeMiddleware = [
