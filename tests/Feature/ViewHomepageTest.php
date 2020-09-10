2c70be349d729ac3687e59e37aad5a0f20566570
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-10 20:24:07 +0200

    Run test without exception handling

--- tests/Feature/ViewHomepageTest.php
+++ tests/Feature/ViewHomepageTest.php
@@ -10,6 +10,8 @@ class ViewHomepageTest extends TestCase
     /** @test */
     public function view_homepage()
     {
+        $this->withoutExceptionHandling();
+
         $response = $this->get('/')
             ->assertStatus(200)
             ->assertSee('Tailwind colors');
