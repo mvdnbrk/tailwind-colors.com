40a6c7290ab74ff79947a2e70952c2b8dc09e282
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-10 09:44:05 +0200

    Update title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Tailwind Colors</title>
+        <title>Tailwind colors</title>
         <link rel="stylesheet" href="{{ mix('css/main.css') }}">
     </head>
     <body class="antialiased bg-gray-100 text-gray-800">
-        <h1 class="p-6 text-4xl text-center">Tailwind Colors</h1>
+        <h1 class="p-6 text-3xl">Tailwind colors</h1>
     </body>
 </html>
