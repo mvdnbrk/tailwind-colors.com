c39c834ded9ded2ff1fcac3b441a89728ca8c21e
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-22 21:34:49 +0200

    Simplifly test

--- tests/Feature/Api/PaletteTest.php
+++ tests/Feature/Api/PaletteTest.php
@@ -10,8 +10,6 @@ class PaletteTest extends TestCase
     /** @test */
     public function it_can_retrieve_the_palette_as_json()
     {
-        $response = $this->get('/api/palette.json');
-
-        $response->assertStatus(Response::HTTP_OK);
+        $this->get('/api/palette.json')->assertStatus(Response::HTTP_OK);
     }
 }
