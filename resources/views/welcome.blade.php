034ff28b22007cec77a5ab68882e1918ec18d2f2
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-25 19:16:58 +0200

    Add shadow

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,12 +12,12 @@
             <h1>Tailwind colors</h1>
         </div>
 
-        <ul class="text-sm font-medium">
+        <ul class="text-sm font-medium shadow">
             @foreach($palette->colors() as $color)
                 <x-color :color="$color">
                     @foreach($palette->shadesOf($color) as $key => $value)
                         <span
-                            class="flex items-end w-24 p-2 text-{{ $color }}-900"
+                            class="flex items-end w-24 p-2 text-{{ $color }}-900 shadow"
                             style="background-color: {{ $value }};"
                         >
                             {{ $key }}
