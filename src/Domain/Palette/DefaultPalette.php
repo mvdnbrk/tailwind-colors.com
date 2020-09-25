4dfe103119e4a5cefd62ead7cf796dbbf7bb9ad6
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-25 18:34:05 +0200

    Add ability to retrieve shades of a color

--- src/Domain/Palette/DefaultPalette.php
+++ src/Domain/Palette/DefaultPalette.php
@@ -20,6 +20,18 @@ final class DefaultPalette implements Arrayable
         return array_keys($this->data);
     }
 
+    public function shadesOf(string $color): array
+    {
+        if (
+            array_key_exists($color, $this->data) &&
+            is_array($this->data[$color])
+        ) {
+            return $this->data[$color];
+        }
+
+        return [];
+    }
+
     public function toArray(): array
     {
         return $this->data;
